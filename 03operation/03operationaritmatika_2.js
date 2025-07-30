9d7606a77d3d4f855270e317132a2e53b33cfb44
yoeda11 <yoeda11@gmail.com>
2025-07-30 07:25:25 +0700

    tambahan operation

---
+++ 03operation/03operationaritmatika_2.js
@@ -0,0 +1,83 @@
+// JavaScript Arithmetic
+// JavaScript Arithmetic Operators
+// Arithmetic operators perform arithmetic on numbers (literals or variables).
+// Operator aritmatika melakukan operasi aritmatika pada angka (literal atau variabel).
+
+//  Operator    Description
+//  +	        Addition
+//  -	        Subtraction
+//  *	        Multiplication
+//  **	        Exponentiation (ES2016)
+//  /	        Division
+//  %	        Modulus (Remainder)
+//  ++	        Increment
+//  --	        Decrement
+
+// Arithmetic Operations
+// A typical arithmetic operation operates on two numbers.
+
+// The two numbers can be literals:
+// Kedua angka tersebut dapat berupa literal:
+let x = 100 + 50; // output 150
+
+// or variables:
+let x = a + b;
+
+// or expressions:
+let x = (100 + 50) * a;
+
+
+// Operators and Operands
+// The numbers (in an arithmetic operation) are called operands.
+// Angka-angka (dalam operasi aritmatika) disebut operan.
+
+// The operation (to be performed between the two operands) is defined by an operator.
+// Operasi (yang akan dilakukan antara dua operan) didefinisikan oleh sebuah operator.
+
+// Operand      Operator        Operand
+// 100          +               50
+
+
+// Adding
+// The addition operator (+) adds numbers:
+
+let x = 5;
+let y = 2;
+let z = x + y;
+
+// Subtracting
+// The subtraction operator (-) subtracts numbers.
+let x = 5;
+let y = 2;
+let z = x - y;
+
+// Multiplying
+// The multiplication operator (*) multiplies numbers.
+let x = 5;
+let y = 2;
+let z = x * y;
+
+// Dividing
+// The division operator (/) divides numbers.
+let x = 5;
+let y = 2;
+let z = x / y;
+
+// Remainder
+// The modulus operator (%) returns the division remainder.
+let x = 5;
+let y = 2;
+let z = x % y;
+
+// Incrementing
+// The increment operator (++) increments numbers.
+let x = 5;
+x++;
+let z = x;
+
+// Decrementing
+// The decrement operator (--) decrements numbers.
+let x = 5;
+x--;
+let z = x;
+
