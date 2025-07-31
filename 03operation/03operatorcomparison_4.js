570c654e86bfdece5aaf99a7e4d31be78517c0bc
yoeda11 <yoeda11@gmail.com>
2025-07-31 13:45:10 +0700

    tambahan comparison

--- 03operation/03operatorcomparison_4.js
+++ 03operation/03operatorcomparison_4.js
@@ -75,5 +75,56 @@ let voteable = (age < 18) ? "Too young":"Old enough"; // Jika usia kurang dari 1
 // Jika variabel usia bernilai di bawah 18 tahun, nilai variabel yang dapat dipilih akan menjadi "Terlalu muda", jika tidak, nilai yang dapat dipilih akan menjadi "Cukup umur".
 
 
+/*
+Comparing Different Types / Membandingkan Tipe yang Berbeda
+Comparing data of different types may give unexpected results. / Membandingkan data dengan tipe yang berbeda dapat memberikan hasil yang tidak terduga.
+When comparing a string with a number, JavaScript will convert the string to a number when doing the comparison. An empty string converts to 0. A non-numeric string converts to NaN which is always false. / Saat membandingkan string dengan angka, JavaScript akan mengonversi string tersebut menjadi angka saat melakukan perbandingan. String kosong akan dikonversi menjadi 0. String non-numerik akan dikonversi menjadi NaN yang selalu bernilai salah.
+*/
+
+/*
+Case                Value	
+2 < 12	            true	
+2 < "12"	        true	
+2 < "John"	        false	
+2 > "John"	        false	
+2 == "John"	        false	
+"2" < "12"	        false	
+"2" > "12"	        true	
+"2" == "12"	        false	
+*/
+
+/*
+When comparing two strings, "2" will be greater than "12", because (alphabetically) 1 is less than 2. / Saat membandingkan dua string, "2" akan lebih besar daripada "12", karena (berdasarkan abjad) 1 lebih kecil daripada 2.
+To secure a proper result, variables should be converted to the proper type before comparison: / Untuk mendapatkan hasil yang tepat, variabel harus dikonversi ke tipe yang tepat sebelum perbandingan:
+*/
+
+age = Number(age);
+if (isNaN(age)) {
+  voteable = "Input is not a number";
+} else {
+  voteable = (age < 18) ? "Too young" : "Old enough";
+}
+
+
+/*
+The Nullish Coalescing Operator (??) / Operator Penggabungan Nullish (??)
+The ?? operator returns the first argument if it is not nullish (null or undefined). / Operator ?? mengembalikan argumen pertama jika bukan nullish (null atau undefined).
+Otherwise it returns the second argument. / Jika tidak, operator ini mengembalikan argumen kedua.
+*/
+
+let name = null;
+let text = "missing";
+let result = name ?? text;
+
+
+/*
+The Optional Chaining Operator (?.) / Operator Rantai Opsional (?.)
+The ?. operator returns undefined if an object is undefined or null (instead of throwing an error). / Operator ?. mengembalikan undefined jika suatu objek tidak terdefinisi atau null (bukannya memunculkan kesalahan).
+*/
+
+// contoh:
+const car = {type:"Fiat", model:"500", color:"white"}; // Create an object:
+document.getElementById("demo").innerHTML = car?.name; // Ask for car name:
+
 
 
