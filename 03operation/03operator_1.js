da7a267a364fc149ada31a106d016e04e24e1756
yoeda11 <yoeda11@gmail.com>
2025-07-31 10:41:19 +0700

    tambahan aritmatika

--- 03operation/03operator_1.js
+++ 03operation/03operator_1.js
@@ -1,16 +1,16 @@
-// Operator JavaScript digunakan untuk melakukan berbagai jenis komputasi matematika dan logika.
+// Javascript operators are used to perform different types of mathematical and logical computations. / Operator JavaScript digunakan untuk melakukan berbagai jenis komputasi matematika dan logika.
 
 /*
 Contoh:
-Operator Penugasan = menetapkan nilai
-Operator Penjumlahan + menambahkan nilai
-Operator Perkalian * mengalikan nilai
-Operator Perbandingan > membandingkan nilai
+The Assignment Operator = assigns values. / Operator Penugasan = menetapkan nilai
+The Addition Operator + adds values. / Operator Penjumlahan + menambahkan nilai
+The Multiplication Operator * multiplies values. / Operator Perkalian * mengalikan nilai
+The Comparison Operator > compares values. / Operator Perbandingan > membandingkan nilai
 */
 
-// Penugasan JavaScript
-// Operator Penugasan (=) menetapkan nilai ke variabel:
-// Contoh Tugas / Penugasan
+// JavaScript Assignment / Penugasan JavaScript
+// The Assignment Operator (=) assigns a value to a variable: / Operator Penugasan (=) menetapkan nilai ke variabel:
+// Assignment Examples / Contoh Tugas / Penugasan
 let xyz = 10; // Tetapkan nilai 10 ke variabel xyz
 console.log(xyz); // Output: 10
 
@@ -18,15 +18,15 @@ let x = 5; // Tetapkan nilai 5 ke x
 let y = 2; // Tetapkan nilai 2 ke y
 let z = x + y; // Tetapkan nilai x + y ke z:
 
-// Penambahan JavaScript
-// Operator Penjumlahan (+) menambahkan angka:
+// JavaScript Addition / Penambahan JavaScript
+// The Addition Operator (+) adds numbers: / Operator Penjumlahan (+) menambahkan angka:
 let a = 5;
 let b = 2;
 let c = a + b;
 console.log(c); // output 7
 
-// Perkalian JavaScript
-// Operator Perkalian (*) mengalikan angka:
+// JavaScript Multiplication / Perkalian JavaScript
+// The Multiplication Operator (*) multiplies numbers: / Operator Perkalian (*) mengalikan angka:
 let d = 5;
 let e = 2;
 let f = d * e;
@@ -36,14 +36,14 @@ console.log(f); // output 10
 /*
 Jenis-jenis Operator JavaScript
 Ada berbagai jenis operator JavaScript:
-Arithmetic Operators
-Assignment Operators
-Comparison Operators
-String Operators
-Logical Operators
-Bitwise Operators
-Ternary Operators
-Type Operators
+Arithmetic Operators / Operator Aritmatika
+Assignment Operators / Operator Penugasan
+Comparison Operators / Operator Perbandingan
+String Operators / Operator String
+Logical Operators / Operator Logika
+Bitwise Operators / Operator Bitwise
+Ternary Operators / Operator Terner
+Type Operators / Operator Tipe
 */
 
 // Operator Aritmatika JavaScript
@@ -52,9 +52,10 @@ Type Operators
 let g = 3;
 let h = (100 + 50) * g; // output 450
 
-// Operator Penugasan JavaScript
-// Operator penugasan menetapkan nilai ke variabel JavaScript.
-// Operator Penugasan Penjumlahan (+=) menambahkan nilai ke variabel.
+
+// JavaScript Assignment Operators / Operator Penugasan JavaScript
+// Assignment operators assign values to JavaScript variables. / Operator penugasan menetapkan nilai ke variabel JavaScript.
+// The Addition Assignment Operator (+=) adds a value to a variable. / Operator Penugasan Penjumlahan (+=) menambahkan nilai ke variabel.
 
 // Contoh Operator Penugasan
 let i = 10;
@@ -70,3 +71,135 @@ Operator    Example    Same As
 %=          x %= y      x = x % y
 **=         x **= y     x = x ** y
 */
+
+
+/*
+JavaScript Comparison Operators / Operator Perbandingan JavaScript
+
+Operator    Description
+==          equal to
+===         equal value and equal type
+!=          not equal
+!==         not equal value or not equal type
+>           greater than
+<           less than
+>=          greater than or equal to
+<=          less than or equal to
+?           ternary operator
+*/
+
+
+/*
+JavaScript String Comparison / Perbandingan String JavaScript
+All the comparison operators above can also be used on strings: / Semua operator perbandingan di atas juga dapat digunakan pada string:
+*/
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result); // output: true
+
+// Note that strings are compared alphabetically: / Perhatikan bahwa string dibandingkan berdasarkan abjad:
+
+let text3 = "20";
+let text4 = "5";
+let hasil = text3 < text4;
+console.log(hasil); // output: false (because "2" is greater than "5")
+
+/*
+JavaScript String Addition / Penambahan String JavaScript
+The + can also be used to add (concatenate) strings: / Tanda + juga dapat digunakan untuk menambahkan (menggabungkan) string:
+*/
+
+let text5 = "John";
+let text6 = "Doe";
+let text7 = text5 + " " + text6; // output "John Doe"
+
+// The += assignment operator can also be used to add (concatenate) strings: / Operator penugasan += juga dapat digunakan untuk menambahkan (menggabungkan) string:
+
+let text8 = "What a very ";
+text8 += "nice day"; // output "What a very nice day"
+
+/*
+The result of text8 will be: / Hasil dari text8 akan menjadi:
+"What a very nice day"
+
+Note / Catatan
+When used on strings, the + operator is called the concatenation operator. / Ketika digunakan pada string, operator + disebut operator penggabungan.
+*/
+
+
+/*
+Adding Strings and Numbers / Menjumlahkan String dan Angka
+Adding two numbers, will return the sum as a number like 5 + 5 = 10. / Menjumlahkan dua angka akan menghasilkan hasil penjumlahan berupa angka, misalnya 5 + 5 = 10.
+Adding a number and a string, will return the sum as a concatenated string like 5 + "5" = "55". / Menjumlahkan angka dan string akan menghasilkan hasil penjumlahan berupa string gabungan, misalnya 5 + "5" = "55".
+*/
+
+let j = 5 + 5; // output 10
+let k = "5" + 5; // output "55"
+let l = "Hello" + 5; // output "Hello5"
+
+/*
+The result of x, y, and z will be: / Hasil dari x, y, dan z adalah:
+
+10
+55
+Hello5
+
+Note / Catatan
+If you add a number and a string, the result will be a string! / Jika Anda menambahkan angka dan string, hasilnya akan berupa string!
+*/
+
+
+/*
+JavaScript Logical Operators / Operator Logika JavaScript
+
+Operator    Description
+&&          logical and
+||          logical or
+!           logical not
+
+Note / Catatan
+Logical operators are fully described in the JS Comparisons chapter. / Operator logika dijelaskan sepenuhnya dalam bab Perbandingan JS.
+*/
+
+
+/*
+JavaScript Type Operators
+
+Operator        Description
+typeof          Returns the type of a variable
+instanceof      Returns true if an object is an instance of an object type
+
+Note / Catatan
+Type operators are fully described in the JS Type Conversion chapter. / Operator tipe dijelaskan sepenuhnya dalam bab Konversi Tipe JS.
+*/
+
+
+/*
+JavaScript Bitwise Operators / Operator Bitwise JavaScript
+Bit operators work on 32 bits numbers. / Operator bit bekerja pada angka 32 bit.
+Any numeric operand in the operation is converted into a 32 bit number. The result is converted back to a JavaScript number.
+Setiap operan numerik dalam operasi ini dikonversi menjadi angka 32 bit. Hasilnya dikonversi kembali menjadi angka JavaScript.
+
+Operator    Description             Example     Same as         Result      Decimal
+&           AND	                    5 & 1       0101 & 0001     0001        1
+|           OR	                    5 | 1       0101 | 0001     0101	    5
+~           NOT	                    ~ 5	        ~0101           1010        10
+^           XOR	                    5 ^ 1       0101 ^ 0001     0100        4
+<<          left shift	            5 << 1      0101 << 1       1010        10
+>>          right shift	            5 >> 1      0101 >> 1       0010	    2
+>>>         unsigned right shift	5 >>> 1	    0101 >>> 1      0010	    2
+
+The examples above uses 4 bits unsigned examples. But JavaScript uses 32-bit signed numbers.
+Because of this, in JavaScript, ~ 5 will not return 10. It will return -6.
+~00000000000000000000000000000101 will return 11111111111111111111111111111010
+Bitwise operators are fully described in the JS Bitwise chapter.
+
+Contoh di atas menggunakan 4 bit unsigned. Namun, JavaScript menggunakan angka bertanda 32-bit.
+Oleh karena itu, dalam JavaScript, ~5 tidak akan mengembalikan 10. Ia akan mengembalikan -6.
+~00000000000000000000000000000101 akan mengembalikan 11111111111111111111111111111010
+Operator bitwise dijelaskan selengkapnya di bab JS Bitwise.
+*/
+
+
--- 03operation/03operatoraritmatika_2.js
+++ 03operation/03operatoraritmatika_2.js
@@ -1,17 +1,20 @@
-// JavaScript Arithmetic
-// JavaScript Arithmetic Operators
-// Arithmetic operators perform arithmetic on numbers (literals or variables).
-// Operator aritmatika melakukan operasi aritmatika pada angka (literal atau variabel).
+/*
+JavaScript Arithmetic / Aritmatika JavaScript
+JavaScript Arithmetic Operators / Operator Aritmatika JavaScript
+Arithmetic operators perform arithmetic on numbers (literals or variables). / Operator aritmatika melakukan aritmatika pada bilangan (literal atau variabel).
+*/
 
-//  Operator    Description
-//  +	        Addition
-//  -	        Subtraction
-//  *	        Multiplication
-//  **	        Exponentiation (ES2016)
-//  /	        Division
-//  %	        Modulus (Remainder)
-//  ++	        Increment
-//  --	        Decrement
+/*
+ Operator   Description
+ +	        Addition
+ -	        Subtraction
+ *	        Multiplication
+ **	        Exponentiation (ES2016)
+ /	        Division
+ %	        Modulus (Remainder)
+ ++	        Increment
+ --	        Decrement
+*/
 
 // Arithmetic Operations
 // A typical arithmetic operation operates on two numbers.
@@ -21,89 +24,84 @@
 let x = 100 + 50; // output 150
 
 // or variables:
-let x = a + b;
+let c = a + b;
 
 // or expressions:
-let x = (100 + 50) * a;
+let y = (100 + 50) * z;
 
+/*
+Operators and Operands
+The numbers (in an arithmetic operation) are called operands. / Angka-angka (dalam operasi aritmatika) disebut operan.
 
-// Operators and Operands
-// The numbers (in an arithmetic operation) are called operands.
-// Angka-angka (dalam operasi aritmatika) disebut operan.
-
-// The operation (to be performed between the two operands) is defined by an operator.
-// Operasi (yang akan dilakukan antara dua operan) didefinisikan oleh sebuah operator.
+The operation (to be performed between the two operands) is defined by an operator. / Operasi (yang akan dilakukan antara dua operan) didefinisikan oleh sebuah operator.
+*/
 
 // Operand      Operator        Operand
 // 100          +               50
 
 
-// Adding
-// The addition operator (+) adds numbers:
+// Adding / Menambahkan / Penjulahan
+// The addition operator (+) adds numbers: / Operator penjumlahan (+) menambahkan angka:
 // contoh:
-let x = 5;
-let y = 2;
-let z = x + y;
+let d = 5;
+let e = 2;
+let f = d + e;
 
-// Subtracting
-// The subtraction operator (-) subtracts numbers.
+// Subtracting / Pengurangan
+// The subtraction operator (-) subtracts numbers. / Operator pengurangan (-) mengurangi angka.
 // contoh:
-let x = 5;
-let y = 2;
-let z = x - y;
+let g = 5;
+let h = 2;
+let i = g - h;
 
-// Multiplying
-// The multiplication operator (*) multiplies numbers.
+// Multiplying / Perkalian
+// The multiplication operator (*) multiplies numbers. / Operator perkalian (*) mengalikan angka.
 // contoh:
-let x = 5;
-let y = 2;
-let z = x * y;
+let j = 5;
+let k = 2;
+let l = j * k;
 
-// Dividing
-// The division operator (/) divides numbers.
+// Dividing / Pembagian
+// The division operator (/) divides numbers. / Operator pembagian (/) membagi angka.
 // contoh:
-let x = 5;
-let y = 2;
-let z = x / y;
+let m = 5;
+let n = 2;
+let o = m / n;
 
-// Remainder
-// The modulus operator (%) returns the division remainder.
+// Remainder / Sisa Bagi
+// The modulus operator (%) returns the division remainder. / Operator modulus (%) mengembalikan sisa pembagian.
 // contoh:
-let x = 5;
-let y = 2;
-let z = x % y;
+let p = 5;
+let q = 2;
+let r = p % q;
 
-// Incrementing
-// The increment operator (++) increments numbers.
+// Incrementing / Penambahan
+// The increment operator (++) increments numbers. / Operator penambahan (++) menambah angka.
 // contoh:
-let x = 5;
-x++;
-let z = x;
+let s = 5;
+s++;
+let t = s;
 
-// Decrementing
-// The decrement operator (--) decrements numbers.
+// Decrementing / Penurunan
+// The decrement operator (--) decrements numbers. / Operator penurunan (--) mengurangi angka.
 // contoh:
-let x = 5;
-x--;
-let z = x;
+let u = 5;
+u--;
+let v = u;
 
 // Exponentiation / Eksponen 
 // Eksponen adalah operasi matematika yang melibatkan bilangan yang dipangkatkan. Dalam istilah sederhana, eksponen menunjukkan berapa kali sebuah angka (basis) dikalikan dengan dirinya sendiri.
 // The exponentiation operator (**) raises the first operand to the power of the second operand.
 // Operator eksponen (**) menaikkan operan pertama ke pangkat operan kedua.
 // contoh:
-let x = 5;
-let z = x ** 2;
+let aa = 5;
+let bb = aa ** 2;
 
-// x ** y produces the same result as Math.pow(x,y):
-// contoh:
-let x = 5;
-let z = Math.pow(x,2);
 // x ** y produces the same result as Math.pow(x,y):
 // x ** y menghasilkan hasil yang sama dengan Math.pow(x,y):
 // contoh:
-let x = 5;
-let z = Math.pow(x,2);
+let cc = 5;
+let dd = Math.pow(cc,2);
 
 // Operator Precedence / Urutan Operator
 /* 
@@ -113,7 +111,7 @@ Secara sederhana, ini menjelaskan operator mana yang akan dieksekusi terlebih da
 // Operator precedence describes the order in which operations are performed in an arithmetic expression.
 // Urutan operator menjelaskan urutan operasi yang dilakukan dalam suatu ekspresi aritmatika.
 // contoh:
-let x = 100 + 50 * 3;
+let gg = 100 + 50 * 3;
 
 /*
 Is the result of example above the same as 150 * 3, or is it the same as 100 + 150?
@@ -133,14 +131,14 @@ Dan (seperti dalam matematika sekolah) prioritas tersebut dapat diubah dengan me
 Saat menggunakan tanda kurung, operasi di dalam tanda kurung dihitung terlebih dahulu:
 */
 // contoh:
-let x = (100 + 50) * 3;
+let hh = (100 + 50) * 3;
 
 // When many operations have the same precedence (like addition and subtraction or multiplication and division), they are computed from left to right:
 // Jika banyak operasi memiliki prioritas yang sama (seperti penjumlahan dan pengurangan atau perkalian dan pembagian), operasi tersebut dihitung dari kiri ke kanan:
 // contoh:
-let x = 100 + 50 - 3; // 147
+let ii = 100 + 50 - 3; // 147
 
 // contoh:
-let x = 100 / 50 * 3;
+let jj = 100 / 50 * 3; // 6
 
 
