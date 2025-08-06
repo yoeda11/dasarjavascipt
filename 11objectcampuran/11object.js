43c296c8291471f123de5982c87f9e41e2006e33
yoeda11 <yoeda11@gmail.com>
2025-08-06 14:36:39 +0700

    objecteventcampuran

---
+++ 11objectcampuran/11object.js
@@ -0,0 +1,27 @@
+
+let ambildata = document.getElementById("run");
+// let ambilnama = document.getElementById("Namaa").value;
+
+
+// Tambahkan event saat form disubmit
+ambildata.addEventListener("submit", function(event) {
+    event.preventDefault(); // mencegah reload halaman
+
+
+
+    // Ambil nilai input
+    let ambilnama = document.getElementById("Namaa").value;
+    // const nama = document.getElementById("nama").value;
+    // const email = document.getElementById("email").value;
+
+
+    // Tampilkan hasilnya
+    // const output = document.getElementById("output");
+    // output.textContent = `Halo ${nama}, email kamu adalah ${email}`;
+    let out = document.getElementById("nama");
+    out.textContent = `hasil ${ambilnama}`;
+
+    });
+
+
+// document.getElementById("nama").innerHTML = ambilnama;
